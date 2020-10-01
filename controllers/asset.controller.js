c00b166b6a8cb26b1ca2f08370db0850285dcc5f
Feisal Mohamud Ahmed <6140236@RTS-365.dk>
2020-10-01 14:28:25 +0200

    initial

---
+++ controllers/asset.controller.js
@@ -0,0 +1,55 @@
+var { Asset } = require("../models/models");
+var saveFile = require("../services/asset");
+
+async function createSingleAsset(req, res, next) {
+	try {
+		let file = saveFile(req.files.file);
+		let asset = await Asset.create({
+			url: process.env.APP_DOMAIN + "/file-bucket/" + file
+		});
+		res.json(asset);
+	} catch (error) {
+		console.error(error);
+		res.status(500).end();
+	}
+}
+
+async function getAllAssets(req, res, next) {
+	try {
+		let assets = await Asset.findAll();
+		res.json(assets);
+	} catch (error) {
+		console.error(error);
+		res.status(500).end();
+	}
+}
+
+async function getSingleAsset(req, res, next) {
+	try {
+		let asset = await Asset.findByPk(req.params.id);
+		res.json(asset);
+	} catch (error) {
+		console.error(error);
+		res.status(500).end();
+	}
+}
+
+async function updateSingleAsset(req, res, next) {
+	try {
+		let asset = await Asset.findByPk(req.params.id);
+		asset.update({
+			url: req.fields.url
+		});
+		res.json(asset);
+	} catch(error) {
+		console.error(error);
+		res.status(500).end();
+	}
+}
+
+module.exports = {
+	createSingleAsset,
+	getAllAssets,
+	getSingleAsset,
+	updateSingleAsset
+};
